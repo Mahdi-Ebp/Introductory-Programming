b6460e85755693fdcc87041a5b6a8f2781157667
mahdi <ebpmahdi@gmail.com>
2024-06-10 20:29:00 +0330

    first program

---
+++ problem1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.Write(c);
+            Console.ReadKey();
+        }
+    }
+}
